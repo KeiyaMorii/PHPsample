c5acfd096a5edd91abf44633e9b43b783506bf7f
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-06 14:32:29 +0900

    GETメソッド

--- get.php
+++ get.php
@@ -1,11 +1,4 @@
 <?php
- $fp = fopen("PHPsample/test.txt", "w");
- if ($fp){
-    fwrite($fp, "書き込みテスト1行目");
-    fwrite($fp, "書き込みテスト2行目");
-    fclose($fp);
-    echo '書き込みました。';
- } else {
-    // ファイルがうまく作成できない等のエラー時、ここへ来る
- }
+$page = $_GET['page'];
+echo 'リクエストされたページは' . $page . 'です。';
 ?>
