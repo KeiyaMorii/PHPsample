613cffd4587f87f5dd90b93cca0c2dcaed096047
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-08 19:34:38 +0900

    改行訂正

--- write.php
+++ write.php
@@ -1,7 +1,7 @@
 <?php
  $fp = fopen("test.txt", "w");
  if ($fp){
-    fwrite($fp, "書き込みテスト1行目¥n");
+    fwrite($fp, "書き込みテスト1行目\r\n");
     fwrite($fp, "書き込みテスト2行目");
     fclose($fp);
     echo '書き込みました。';
